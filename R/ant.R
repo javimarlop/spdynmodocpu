105d8f6944a8a894d675c38df58dc8f5fbe58e1b
arevaju <juan.arevalo.torres@gmail.com>
2015-11-07 18:36:48 +0100

    testing

---
+++ R/ant.R
@@ -0,0 +1,6 @@
+ant<-function(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm){
+#run_model(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm)
+#inlineleafmap(year=2008)
+antonio<-get(antonio)
+return(antonio)
+}
---
+++ R/run_plot3.R
@@ -0,0 +1,4 @@
+run_plot3<-function(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm){
+run_model(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm)
+inlineleafmap(year=2008)
+}
