1e8362c79dd0a177efdeb5849f888c9a3dd75cd1
Javier Martinez <javier.martinez@um.es>
2015-11-11 12:07:32 +0100

    testing toJSON failed

--- R/inlineleafmap.R
+++ R/inlineleafmap.R
@@ -4,12 +4,14 @@ inlineleafmap <- function(year=2008){
  # a$height <- 400
 #paste(capture.output(a))
 require(htmltools)
+require(jsonlite)
 #dok <- tags$html(output)
 ##doc <- HTML(output)
   ##paste(capture.output(doc), collapse ='\n')
 ##paste(capture.output(output), collapse ='\n')
 save_html(output,'tmp.html')
 z<-includeText('tmp.html')
+z2<-toJSON(z)
 return(z)
 }
 
--- R/run_plot3.R
+++ R/run_plot3.R
@@ -1,8 +1,8 @@
 run_plot3<-function(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm){
 run_model(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm)
 #ant(rnd=rnd,pgr_rb=pgr_rb,pgr_sm=pgr_sm)
-###aa0<-inlineleafmap(year=2008)
-output <- plotr_leaflet(year=2008)
+aa0<-inlineleafmap(year=2008)
+#output <- plotr_leaflet(year=2008)
 #a1a<-gsub('<html>','<html><body>', aa, fixed=TRUE)
 #a2a<-gsub('</html>','</body></html>', a1a, fixed=TRUE)
 #a0<-paste(capture.output(aa), collapse ='\n')
@@ -15,5 +15,5 @@ output <- plotr_leaflet(year=2008)
 #a<-gsub("\n", "", aa2, fixed=TRUE)
 #paste(aa2)
 #return(aa)
-output
+aa0
 }
